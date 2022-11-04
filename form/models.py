2b0f05a1db0541dec8e599a37289f73b3094caf1
Erhan <erhancitil94@gmail.com>
2022-11-04 08:28:10 +0100

    Templates folder & view for the index page

--- form/models.py
+++ form/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Contactdata(models.Model):
+    naam = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    onderwerp = models.CharField(max_length=255)
+    bericht = models.CharField(max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'contactdata'
